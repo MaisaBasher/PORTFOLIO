d9efa72f41fa48a78672abd996f29ea30cf8ae19
Maisa Basher <maisacompete@gmail.com>
2023-02-06 13:10:27 -0500

    started

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import dp from './img/me2.jpg'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Maisa Basher</h1>
+        <img className="DP" src={dp}  alt="DP" />
+      </header>
+     
+    </div>
+  );
+}
+
+export default App;
